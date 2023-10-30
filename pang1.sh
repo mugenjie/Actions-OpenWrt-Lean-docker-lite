2480b4161efb5e7bb02359410fb5fe57b10048d2
mugenjie <85976222+mugenjie@users.noreply.github.com>
2023-10-30 19:18:16 +0800

    Initial commit

---
+++ pang1.sh
@@ -0,0 +1,6 @@
+sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+#sed -i 's/KERNEL_PATCHVER:=6.1/KERNEL_PATCHVER:=5.15/g' ./target/linux/x86/Makefile
+sed -i 's/KERNEL_PATCHVER:=6.1/KERNEL_PATCHVER:=5.4/g' ./target/linux/x86/Makefile
+git clone https://github.com/xiaorouji/openwrt-passwall-packages.git package/passwall/packages
+git clone https://github.com/xiaorouji/openwrt-passwall.git package/PWluci
+git clone https://github.com/xiaorouji/openwrt-passwall2.git package/passwall2
